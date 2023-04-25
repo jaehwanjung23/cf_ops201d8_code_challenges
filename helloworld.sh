05833d2b63f00e72139d09a8637ee09cbd471903
jaehwanjung23 <jaehwanjung23@gmail.com>
2023-04-25 19:28:38 +0000

    hello world.sh

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script Name:                  helloworld
+# Author:                       Jason J
+# Date of latest revision:      04/25/2023
+# Purpose:                      Print a phrase.
+
+# Declaration of variables
+# Declaration of functions
+
+
+# Main
+echo "hello world"
+
+
+# End
